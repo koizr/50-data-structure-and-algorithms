c6d11b2332439f052ffe9e5318cc98c43d87c3e0
koizumin <koizumi.ryo.dev@gmail.com>
2019-03-30 18:34:16 +0900

    fix: use toEqual instead of toBe and JSON.stringify for test array

--- 1/1.1.test.js
+++ 1/1.1.test.js
@@ -1,11 +1,11 @@
 import resolver from "./1.1"
 
 test("足りない整数なし", () => {
-    expect(JSON.stringify(resolver([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])))
-        .toBe(JSON.stringify([]))
+    expect(resolver([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+        .toEqual([])
 })
 
 test("5, 6, 10 が足りない", () => {
-    expect(JSON.stringify(resolver([1, 2, 3, 4, 7, 8, 9])))
-        .toBe(JSON.stringify([5, 6, 10]))
+    expect(resolver([1, 2, 3, 4, 7, 8, 9]))
+        .toEqual([5, 6, 10])
 })
--- 1/1.2.test.js
+++ 1/1.2.test.js
@@ -1,6 +1,6 @@
 import resolver from "./1.2"
 
 test("Dupulicate 2 and 5", () => {
-    expect(JSON.stringify(resolver([1, 2, 2, 3, 8, 6, 9, 5, 7, 2, 2, 4, 5, 0, 5])))
-        .toBe(JSON.stringify([2, 5]))
+    expect(resolver([1, 2, 2, 3, 8, 6, 9, 5, 7, 2, 2, 4, 5, 0, 5]))
+        .toEqual([2, 5])
 })
